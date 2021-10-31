36f62d5c966fec996b0733aa64f81b36eaa93b80
ashpurple <ashpurple9@gmail.com>
2021-10-31 16:33:04 +0900

    Restructure files and functions

--- LAB3_SearchEngine/run.py
+++ LAB3_SearchEngine/run.py
@@ -1,24 +1,8 @@
-from LAB3_SearchEngine.infoRetrieval import *
+from infoRetrieval import *
 import pandas as pd
 
-def run():
-  print("Process Query")
-  qDocs = parseQuery("LAB3_SearchEngine/Cranfield_Collection/cran.qry")
-  qToks = tokenize(qDocs, "query")
-  qDic = organize(qToks, "query")
-  # qIdf = idf(qDic, len(qDocs))
-  # qTf = queryTf(qToks)
-
-
-  print("Process Abstract Docs")
-  absDocs = parseAbsDocs("LAB3_SearchEngine/Cranfield_Collection/cran.all.1400")
-  absToks = tokenize(absDocs, "abstract")
-  absDic = organize(absToks, "abstract")
-  # absIdf = idf(absDic, len(absDocs))
-  # absTf = abstractTf(absDic,len(absDocs))
-
-
-  # Construct Inverted Index  TODO: CountVectorizer 안쓰고 tokenize() 구현되어 있는 거 바탕으로 했습니다.
+def constructInvertedIndex(absDic):
+  # Construct Inverted Index
   termList = list(absDic.keys())
   docList = []
   docValues = list(absDic.values())
@@ -34,20 +18,51 @@ def run():
   invertedIndexDF = pd.DataFrame({'Term':list(invertedIndex.keys()),
                                  'Document':list(invertedIndex.values())})
 
-  # print(invertedIndexDF)
   invertedIndexDF = invertedIndexDF[127:].reset_index()
   invertedIndexDF = invertedIndexDF.drop(['index'], axis=1)
   print(invertedIndexDF.head(30))
+  return invertedIndexDF
 
+def run():
+  # Docmument preprocessing
+  print("==Process Abstract Docs==")
+  absDocs = parseAbsDocs("LAB3_SearchEngine/Cranfield_Collection/cran.all.1400.txt")
+  absToks = tokenize(absDocs, "abstract")
+  absDic = organize(absToks, "abstract")
+  absIdf = idf(absDic, len(absDocs))
+  absTf = abstractTf(absDic,len(absDocs))
 
-  '''
-  print("Process Scoring" )
+  # Construct Inverted Index
+  print("==Process costruct inverted index==")
+  invertedIndex = constructInvertedIndex(absDic)
+
+  # Query preprocessing
+  print("==Process Query==")
+  qDocs = parseQuery("LAB3_SearchEngine/Cranfield_Collection/cran.qry.txt")
+  qToks = tokenize(qDocs, "query")
+  qDic = organize(qToks, "query")
+  qIdf = idf(qDic, len(qDocs))
+  qTf = queryTf(qToks)
+
+  # Evaluation
+  print("===Process Scoring===" )
   scoreList = score(qToks, absTf, absIdf, qTf, qIdf)
+  #print(scoreList)
 
-  print("Saving the Result")
+  # Output
+  print("===Saving the Result===")
   createOutput("output.txt", scoreList)
-  print("all done! results are saved in 'output.txt' file")
-  '''
+  print("All done!")
+
+def createOutput(fileName, scores):
+  with open(fileName, "w") as f:
+    for score in scores:
+      for tup in score:
+        string = ""
+        for t in tup:
+          string = string + str(t) + " "
+        f.write(string + "\n")
+    f.close()
 
 if __name__ == "__main__":
   run()
